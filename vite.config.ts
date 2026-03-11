7da4d186d3ea1a1ac755d6bfef6682829fe6a316
Theo <theobbb64@gmail.com>
2026-03-11 09:59:23 -0400

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
+
+export default defineConfig({
+	plugins: [tailwindcss(), sveltekit()]
+});
