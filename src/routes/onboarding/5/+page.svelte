5ebccc47fb414ccdaf702f39760adf615c269077
Theo <theobbb64@gmail.com>
2026-03-11 18:42:28 -0400

    fix: update link in onboarding page to point to correct route

--- src/routes/onboarding/5/+page.svelte
+++ src/routes/onboarding/5/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="mb-4x flex items-center justify-between">
 	<Title icon="icon-[ri--user-line]">Famille</Title>
-	<a href="/app" class="flex items-center gap-1">
+	<a href="/app/1" class="flex items-center gap-1">
 		Sauter
 		<span class="icon-[ri--arrow-right-long-line]"></span>
 	</a>
